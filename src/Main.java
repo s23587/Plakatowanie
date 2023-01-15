9364a1de9a8d54a60c006928dfe5f27dbaf1cf8a
s23587 <s23587@pjwstk.edu.pl>
2023-01-15 23:16:15 +0100

    everything works properly

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ import java.util.Stack;
 
 public class Main {
 
-    public static int numberOfPosters=0;
+    public static int numberOfPosters = 0;
 
     public static void main(String[] args) {
 
         BufferedReader bufferedReader;
         String line;
-        int numberOfBuildings=0,currentHeight;
+        int numberOfBuildings = 0, currentHeight;
         Stack<Integer> heights = new Stack<>();
 
         try {
@@ -17,51 +17,36 @@ public class Main {
 
             numberOfBuildings = Integer.parseInt(bufferedReader.readLine());
 
-            while ((line = bufferedReader.readLine()) != null){
+            while ((line = bufferedReader.readLine()) != null) {
 
                 currentHeight = Integer.parseInt(line.split(" ")[1]);
 
-                if(heights.empty()){
+                if (heights.empty()) {
                     heights.push(currentHeight);
-                }else if(currentHeight>heights.peek()){
                     numberOfPosters++;
+                } else if (currentHeight > heights.peek()) {
                     heights.push(currentHeight);
-                }
-
-                if(!heights.empty()) {
-                    if(currentHeight<heights.peek()) {
+                    numberOfPosters++;
+                } else if (currentHeight < heights.peek()) {
+                    while (!heights.empty() && heights.peek() > currentHeight) {
+                        heights.pop();
+                    }
+                    if ((!heights.empty() && heights.peek() < currentHeight) || heights.isEmpty()){
+                        heights.push(currentHeight);
                         numberOfPosters++;
-                        recursionCheck(currentHeight, heights);
                     }
                 }
-
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if(numberOfBuildings==1) {
+        if (numberOfBuildings == 1) {
             System.out.println("Minimalna liczba plakatów to: " + numberOfBuildings);
-        }else {
+        } else {
             System.out.println("Minimalna liczba plakatów to: " + numberOfPosters);
         }
 
     }
-
-    public static void recursionCheck(int currentHeight, Stack<Integer> stack){
-        if (!stack.empty()) {
-            if (currentHeight < stack.peek()) {
-                stack.pop();
-                if (!stack.empty()) {
-                    if (stack.peek() < currentHeight) {
-                        stack.push(currentHeight);
-                    }
-                }
-                recursionCheck(currentHeight, stack);
-            }
-        }
-    }
-
 }
-
